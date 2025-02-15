a6ce851b6ef8fab649a25b738de588181548c071
bezardo <132924156+bezardo@users.noreply.github.com>
2025-02-15 11:49:57 +0530

    Update 01-string-substring.py

--- Day-02/examples/01-string-substring.py
+++ Day-02/examples/01-string-substring.py
@@ -2,3 +2,10 @@ text = "Python is awesome"
 substring = "is"
 if substring in text:
     print(substring, "found in the text")
+
+---
+
+text = "Python is awesome"
+substring = text[text.find('is'):]  # Get "is" and everything after it
+if substring in text:
+ print(substring, "found in the text")
