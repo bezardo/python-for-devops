7f4bd3b72bed166e1039b4f530cbd5d5f8880ba3
bezardo <132924156+bezardo@users.noreply.github.com>
2025-02-15 12:24:53 +0530

    Update 03-regex-replace.py

--- Day-02/examples/03-regex-replace.py
+++ Day-02/examples/03-regex-replace.py
@@ -7,3 +7,20 @@ replacement = "red"
 
 new_text = re.sub(pattern, replacement, text)
 print("Modified text:", new_text)
+
+
+---
+
+import re
+text = "serach korone in this korone koro"
+pattern= r"koron*"
+replace = "fubuki"
+res = re.sub(pattern,replace,text)
+if res:
+    print("changed to fubuchan", res)
+else:
+    print("nope")  
+
+op : changed to fubuchan serach fubukie in this fubukie fubuki
+
+
