0860acceb5d960daf2a74762647351a5c6ce30a6
bezardo <132924156+bezardo@users.noreply.github.com>
2025-02-15 10:26:37 +0530

    Update 01-string-replace.py

--- Day-02/examples/01-string-replace.py
+++ Day-02/examples/01-string-replace.py
@@ -1,3 +1,6 @@
 text = "Python is awesome"
 new_text = text.replace("awesome", "great")
 print("Modified text:", new_text)
+
+#replace() doesn’t work with indexes – it replaces characters, not positions.
+
