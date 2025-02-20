168e26effd1f2d0788747db0a83f542a124943b7
bezardo <132924156+bezardo@users.noreply.github.com>
2025-02-20 10:29:38 +0530

    Update 03-list-files-in-folders.py

--- Day-10/03-list-files-in-folders.py
+++ Day-10/03-list-files-in-folders.py
@@ -3,7 +3,7 @@ import os
 def list_files_in_folder(folder_path):
     try:
         files = os.listdir(folder_path)
-        return files, None
+        return files, None  # returning as a tuple
     except FileNotFoundError:
         return None, "Folder not found"
     except PermissionError:
@@ -13,7 +13,7 @@ def main():
     folder_paths = input("Enter a list of folder paths separated by spaces: ").split()
     
     for folder_path in folder_paths:
-        files, error_message = list_files_in_folder(folder_path)
+        files, error_message = list_files_in_folder(folder_path)  # refer bottom x,y example we are unpacking tuple data and assigning as vars
         if files:
             print(f"Files in {folder_path}:")
             for file in files:
