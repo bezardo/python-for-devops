d254c20072a4005f3ecf66c43aa6f16f61a45904
bezardo <132924156+bezardo@users.noreply.github.com>
2025-02-28 22:14:33 +0530

    Update 04-practicals.py

--- Day-11/04-practicals.py
+++ Day-11/04-practicals.py
@@ -13,3 +13,30 @@ def get_server_status(server_name):
 server_name = 'server2'
 status = get_server_status(server_name)
 print(f"{server_name} status: {status}")
+
+
+###
+instances_response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+active_instance_ids = set()
+
+for reservation in instances_response['Reservations']:  
+    for instance in reservation['Instances']:  
+        active_instance_ids.add(instance['InstanceId'])
+------
+AWS Response Data (Simplified)
+{
+  "Reservations": [
+    {
+      "Instances": [
+        {"InstanceId": "i-1111"},
+        {"InstanceId": "i-2222"}
+      ]
+    },
+    {
+      "Instances": [
+        {"InstanceId": "i-3333"},
+        {"InstanceId": "i-1111"}  # Duplicate instance
+      ]
+    }
+  ]
+}
